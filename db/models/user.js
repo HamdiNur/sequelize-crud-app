0c429ca0d5fc8177108a54ad7b9bc26b1007ec70
HamdiNur <hamdoonur26@gmail.com>
2025-09-17 14:08:48 +0300

    Update User model: add paranoid, hooks, and associations

--- db/models/user.js
+++ db/models/user.js
@@ -1,58 +1,99 @@
 'use strict';
-const {  Model, Sequelize, DataTypes} = require('sequelize');
-const bcrypt= require('bcrypt')
-const sequelize=require('../../config/database');
+const { Sequelize, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../../config/database');
+const AppError = require('../../utils/appError');
+const project = require('./project');
 
-module.exports=sequelize.define('user',{
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: DataTypes.INTEGER
-      },
-      userType: {
-     type: DataTypes.ENUM('0', '1', '2'), // add the values you want
-      },
-      firstName: {
-        type: DataTypes.STRING
-      },
-      lastName: {
-        type: DataTypes.STRING
-      },
-      email: {
-        type: DataTypes.STRING
-      },
-      password: {
-        type: DataTypes.STRING
-      },
-      confirmPassword:{
-         type:DataTypes.VIRTUAL,
-         set(value){
-          if(value===this.password){
-            const hashPassword=bcrypt.hashSync(value,10);
-            this.setDataValue('password',hashPassword);
+const user = sequelize.define('user', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+    allowNull: false
+  },
 
-          }else{
-            throw new Error("Password and confirm Password must be the same ")
-          }
-         }
+  userType: {
+    type: DataTypes.ENUM('0','1','2'),
+    allowNull: false,
+    validate: {
+      notNull: { msg: 'User type cannot be null' },
+      notEmpty: { msg: 'User type cannot be empty' }
+    }
+  },
 
-      },
-      createdAt: {
-        allowNull: false,
-        type: DataTypes.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: DataTypes.DATE
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notNull: { msg: 'First name cannot be null' },
+      notEmpty: { msg: 'First name cannot be empty' }
+    }
+  },
+
+  lastName: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notNull: { msg: 'Last name cannot be null' },
+      notEmpty: { msg: 'Last name cannot be empty' }
+    }
+  },
+
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: { msg: 'Email must be unique' },
+    validate: {
+      notNull: { msg: 'Email cannot be null' },
+      notEmpty: { msg: 'Email cannot be empty' },
+      isEmail: { msg: 'Must be a valid email address' }
+    }
+  },
+
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notNull: { msg: 'Password cannot be null' },
+      notEmpty: { msg: 'Password cannot be empty' },
+      len: { args: [6, 100], msg: 'Password must be at least 6 characters long' }
+    }
+  },
+
+  confirmPassword: {
+    type: DataTypes.VIRTUAL,
+    set(value) {
+      if (value !== this.password) {
+        throw new AppError('Password and confirm password must be the same', 400);
       }
-      ,deletedAt:{
-        type:DataTypes.DATE,
+    }
+  },
+
+  createdAt: { type: DataTypes.DATE, allowNull: false },
+  updatedAt: { type: DataTypes.DATE, allowNull: false },
+  deletedAt: { type: DataTypes.DATE }
+
+}, {
+  paranoid: true,
+  freezeTableName: true,
+  modelName: 'user',
+
+  hooks: {
+    beforeCreate: (user) => {
+      if (user.password) {
+        user.password = bcrypt.hashSync(user.password, 10);
       }
     },
-  {
-    paranoid: true,
-    freezeTableName:true,
-    modelName:"username"
+    beforeUpdate: (user) => {
+      if (user.changed('password')) {
+        user.password = bcrypt.hashSync(user.password, 10);
+      }
+    }
+  }
+});
 
-  })
+user.hasMany(project,{foreignKey:'createdBy'})
+project.belongsTo(user,{foreignKey:'createdBy'})
+
+module.exports = user;
